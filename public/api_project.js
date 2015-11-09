0ef5e5f96cc785a1fb585168d7fd007cb6870aba
valik-pc <valik.v1per@gmail.com>
2015-11-09 22:57:11 +0200

    update doc

--- public/api_project.js
+++ public/api_project.js
@@ -3,12 +3,12 @@ define({
   "version": "0.0.1",
   "description": "Distance learning documentation",
   "title": "Distance learning documentation",
-  "url": "http://distance-learning.local",
+  "url": "http://distance-learning.herokuapp.com/",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-11-08T19:36:54.873Z",
+    "time": "2015-11-09T20:57:02.538Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
