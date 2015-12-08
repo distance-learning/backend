f2fe8989658de46acadaf53aa4511ad2d856182c
valik-pc <valik.v1per@gmail.com>
2015-12-09 00:44:33 +0200

    create Faculty factory and write tests for get faculties and get faculty by slug

---
+++ database/factories/FacultyFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+$factory->define(App\Faculty::class, function (Faker\Generator $faker) {
+    return [
+        'name'          => $faker->title,
+        'description'   => $faker->text,
+        'avatar'        => '',
+    ];
+});
+
+$factory->defineAs(App\Faculty::class, 'fotius', function () {
+    return [
+        'name'          => 'ФОТИУС',
+        'description'   => 'Опис факультету ФОТИУС',
+        'avatar'        => ''
+    ];
+});
