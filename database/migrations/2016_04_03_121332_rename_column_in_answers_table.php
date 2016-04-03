76fe56925e88014297a04e52f35470431c47afaf
valik-pc <valik.v1per@gmail.com>
2016-04-03 15:27:34 +0300

    fix migration

--- database/migrations/2016_04_03_121332_rename_column_in_answers_table.php
+++ database/migrations/2016_04_03_121332_rename_column_in_answers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class RenameColumnInQuestionsTable extends Migration
+class RenameColumnInAnswersTable extends Migration
 {
     /**
      * Run the migrations.
