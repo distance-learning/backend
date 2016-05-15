bede55846032714be19dff0d9080e564b00742ff
valik-pc <valik.v1per@gmail.com>
2016-05-15 19:10:11 +0300

    enable all tests

--- tests/Controllers/Admin/AdminCoursesTest.php
+++ tests/Controllers/Admin/AdminCoursesTest.php
@@ -1,18 +1,20 @@
 <?php
-//
-//
-//class AdminCoursesTest extends TestCase
-//{
-//    public function testGetCourses()
-//    {
-//        $request = $this->get('/api/admin/courses', [
-//            'Authorization' => 'Bearer ' . $this->getAuthUserToken(),
-//        ]);
-//
-//        $content = (array) json_decode($request->response->getContent());
-//        $statusCode = $request->response->getStatusCode();
-//
-//        $this->assertEquals(200, $statusCode);
-//        $this->assertCount(0, $content['data']);
-//    }
-//}
+
+
+class AdminCoursesTest extends TestCase
+{
+    public function testGetCourses()
+    {
+        $this->checkAuthPermission('get', '/api/admin/courses');
+
+        $request = $this->get('/api/admin/courses', [
+            'Authorization' => 'Bearer ' . $this->getAuthUserToken(),
+        ]);
+
+        $content = (array) json_decode($request->response->getContent());
+        $statusCode = $request->response->getStatusCode();
+
+        $this->assertEquals(200, $statusCode);
+        $this->assertCount(0, $content['data']);
+    }
+}
