6d73f924e184d70e77ab6eb02bb3dc817448a797
valik-pc <valik.v1per@gmail.com>
2016-05-29 12:31:42 +0300

    fix remove students from group

--- app/Http/Controllers/Admin/GroupsController.php
+++ app/Http/Controllers/Admin/GroupsController.php
@@ -214,7 +214,9 @@ class GroupsController extends Controller
      */
     public function removeStudentsFromGroupAction(Request $request, Group $group)
     {
-        foreach ($request->get('students') as $student) {
+        $students = User::findMany($request->get('students'));
+
+        foreach ($students as $student) {
             if ($student->isStudent()) {
                 $student->group_id = null;
                 $student->save();
