1e0aef8f679f567a346da83a020ad4d5b0d8d224
valik-pc <valik.v1per@gmail.com>
2016-06-09 21:34:22 +0300

    update events

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -39,13 +39,13 @@ class EventsController extends Controller
             $events = Event::where('recipient_id', $user->id)
                 ->where('deadline', '>=', $from_date)
                 ->where('deadline', '<=', $to_date)
-                ->with('attachment')
+                ->with('attachment.attachment')
                 ->get();
         } else {
             $events = Event::where('sender_id', $user->id)
                 ->where('deadline', '>=', $from_date)
                 ->where('deadline', '<=', $to_date)
-                ->with('attachment')
+                ->with('attachment.attachment')
                 ->get();
         }
 
