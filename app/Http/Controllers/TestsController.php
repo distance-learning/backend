bdabd58042469835881e96a8d28b4a14e75f4ac6
valik-pc <valik.v1per@gmail.com>
2016-06-11 18:19:39 +0300

    update get test for passing

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -175,9 +175,13 @@ class TestsController extends Controller
      */
     public function getTestsForPassingAction(Request $request, Test $test)
     {
-        $test = $test->whereHas('questions', function ($query) {
-            return $query->where('is_active', true);
-        })->load('questions.answers')->shuffle()->splice(0, $test->count_questions);
+        $test = $test->load('questions.answers')->shuffle()->splice(0, $test->count_questions);
+
+        $questions = $test->questions->filter(function ($item) {
+            return $item->is_active;
+        });
+
+        $test->questions = $questions;
 
         return response()->json($test);
     }
