04007458889fe6dbbd01e5f48eec73aa8dcfe448
valik-pc <valik.v1per@gmail.com>
2016-06-12 11:54:06 +0300

    update tests controller #2

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -175,16 +175,15 @@ class TestsController extends Controller
      */
     public function getTestsForPassingAction(Request $request, Test $test)
     {
-        $questions = $test->questions->where('is_active', true)->load('answers')->shuffle()->splice(0, $test->count_questions);
+//        $questions = $test->questions->where('is_active', true)->load('answers')->shuffle()->splice(0, $test->count_questions);
 
         $test = Test::whereHas('questions', function ($query) {
-            $query->answers;
             return $query->where('is_active', true);
-        })->find($test->id)->shuffle()->splice(0, $test->count_questions);
+        })->find($test->id)->load('questions.answers')->shuffle()->splice(0, $test->count_questions);
         //TODO need remove
 
         return response()->json([
-            "questions" => $questions,
+//            "questions" => $questions,
             "test" => $test
         ]);
     }
